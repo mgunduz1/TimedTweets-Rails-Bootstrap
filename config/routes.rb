c2286f29b2286a59e0dcb13b8b1d88e25701d6b2
mgunduz1 <mgunduz114118@gmail.com>
2021-04-08 14:53:58 +0300

    add sign-up page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   get "about", to: "about#index"
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
   root "main#index"
 
 end
